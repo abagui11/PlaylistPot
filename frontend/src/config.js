570ef2e9a408f130b280020dfd60a02bc51a4c00
abagui11 <bagui.arpan@gmail.com>
2024-11-04 13:21:56 -0500

    checking if config is updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const BACKEND_URL =
-    process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV === 'prod'
         ? 'http://localhost:3001'
-        : 'https://playlist-pot.vercel.app';
+        : 'http://54.147.156.144:8080';
 
 export default BACKEND_URL;
