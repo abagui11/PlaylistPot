a29d26865a486f3c5092b28318a7869cb1ecf6fb
abagui11 <bagui.arpan@gmail.com>
2024-11-13 03:33:13 -0500

    frontend change

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -4,7 +4,8 @@ import React from 'react';
 const LoginPage = ({ onLogin }) => {
   return (
     <div>
-      <h1>Login to Spotify</h1>
+      <h1>Playlist Pot</h1>
+      <h3>Throw your favorite artists, albums, and tracks into the pot and make a playlist out of them easily</h3>
       <button onClick={onLogin}>Login with Spotify</button>
     </div>
   );
